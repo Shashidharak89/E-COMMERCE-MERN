7cf5d9171fcb040a9a8b3989dd1eb1770c2b4ae1
Shashidharak89 <shashidharak334@gmail.com>
2025-06-27 20:24:57 +0530

    login details stores in context variables

--- src/components/auth/Login.jsx
+++ src/components/auth/Login.jsx
@@ -4,37 +4,44 @@ import './styles/auth.css';
 import SampleContext from '../../contexts/SampleContext';
 
 const Login = () => {
-  const [form, setForm] = useState({ email: '', password: '' });
-  const [message, setMessage] = useState('');
-const {URL}=useContext(SampleContext);
+    const [form, setForm] = useState({ email: '', password: '' });
+    const [message, setMessage] = useState('');
+    const { URL, userId, setUserId,
+        username, setUsername,
+        mail, setMail,
+        islogin, setIslogin } = useContext(SampleContext);
 
-  const handleChange = (e) =>
-    setForm({ ...form, [e.target.name]: e.target.value });
+    const handleChange = (e) =>
+        setForm({ ...form, [e.target.name]: e.target.value });
 
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    try {
-      const res = await axios.post(URL+'/api/auth/login', form);
-      console.log(res.data);
-      setMessage('Login successful ✅');
-      localStorage.setItem('token', res.data.token);
-    } catch (err) {
-      console.error(err);
-      setMessage('Login failed ❌');
-    }
-  };
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        try {
+            const res = await axios.post(URL + '/api/auth/login', form);
+            console.log(res.data);
+            setUsername(res.data.user.name);
+            setIslogin(true);
+            setMail(res.data.user.email);
+            setUserId(res.data.user._id);
+            setMessage('Login successful ✅');
+            localStorage.setItem('token', res.data.token);
+        } catch (err) {
+            console.error(err);
+            setMessage('Login failed ❌');
+        }
+    };
 
-  return (
-    <div className="auth-container">
-      <h2>Login</h2>
-      <form onSubmit={handleSubmit}>
-        <input name="email" type="email" placeholder="Email" onChange={handleChange} required />
-        <input name="password" type="password" placeholder="Password" onChange={handleChange} required />
-        <button type="submit">Login</button>
-      </form>
-      <p>{message}</p>
-    </div>
-  );
+    return (
+        <div className="auth-container">
+            <h2>Login</h2>
+            <form onSubmit={handleSubmit}>
+                <input name="email" type="email" placeholder="Email" onChange={handleChange} required />
+                <input name="password" type="password" placeholder="Password" onChange={handleChange} required />
+                <button type="submit">Login</button>
+            </form>
+            <p>{message}</p>
+        </div>
+    );
 };
 
 export default Login;
