0235c9944433b262576b9fb9d76796fabf0f79a9
Shashidharak89 <shashidharak334@gmail.com>
2025-06-27 20:14:31 +0530

    contexts

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,13 @@
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom'; // Router
-import { AuthProvider } from './context/AuthContext'; // Auth Context provider
-import './index.css';
-import App from './App.jsx';
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import { SampleState } from "./contexts/SampleState.jsx";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <AuthProvider>           {/* Wrap with AuthProvider */}
-      <BrowserRouter>        {/* Then wrap with Router */}
-        <App />
-      </BrowserRouter>
-    </AuthProvider>
+    <SampleState>
+      <App />
+    </SampleState>
   </StrictMode>
 );
