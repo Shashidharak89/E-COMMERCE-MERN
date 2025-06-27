14db1c3b6c09ad13b299e74e27d47daf104b209d
Shashidharak89 <shashidharak334@gmail.com>
2025-06-27 20:06:25 +0530

    context

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'; // ✅ import router
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom'; // Router
+import { AuthProvider } from './context/AuthContext'; // Auth Context provider
+import './index.css';
+import App from './App.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter> {/* ✅ Wrap App in Router */}
-    <App />
-    </BrowserRouter>
-  </StrictMode>,
+    <AuthProvider>           {/* Wrap with AuthProvider */}
+      <BrowserRouter>        {/* Then wrap with Router */}
+        <App />
+      </BrowserRouter>
+    </AuthProvider>
+  </StrictMode>
 );
